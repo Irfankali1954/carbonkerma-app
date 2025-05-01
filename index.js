fca81d90c8a491533bbd7edf8de2693f096bdf4b
Irfankali1954 <irfankali@gmail.com>
2025-05-01 15:33:07 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>CarbonKerma CO₂ Tracker</h1>
-      <p>This is your MVP frontend. We'll soon connect this to your emissions backend.</p>
+      <p>This is your MVP frontend. Backend integration is coming next.</p>
     </div>
   );
 }
